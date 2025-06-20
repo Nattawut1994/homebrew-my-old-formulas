489a46cd66d86f1f27741003f6af48b4605c77f3
Nattawut Kongtan <nattawut.k@tidlor.com>
2025-06-20 17:31:20 +0700

    Add edencommon [OLD_VERSION_HERE] formula

---
+++ Formula/edencommon.rb
@@ -0,0 +1,74 @@
+class Edencommon < Formula
+  desc "Shared library for Watchman and Eden projects"
+  homepage "https://github.com/facebookexperimental/edencommon"
+  url "https://github.com/facebookexperimental/edencommon/archive/refs/tags/v2024.10.07.00.tar.gz"
+  sha256 "aab1b5fb53f1a8b06f4b990c6d062ce956aa0f87a38165c818cc096b6161104c"
+  license "MIT"
+  head "https://github.com/facebookexperimental/edencommon.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "0f23ce0c47437438a4d7c5f18ac136390f1c83a34076577fcb6a2da410a31b6e"
+    sha256 cellar: :any,                 arm64_sonoma:  "dd747941483408ea51ee93837b85bb4b0a8efb4e5efb06a545e1fd11c56d30c3"
+    sha256 cellar: :any,                 arm64_ventura: "594104ed1dd9e3ad7359945ac5778cc069c1c4c1252b06fcfcf82e9ef1bae31d"
+    sha256 cellar: :any,                 sonoma:        "5f2dfdee49985381b20c7c0562d447fc14e16ba353094d6f2184e1b1e96b0379"
+    sha256 cellar: :any,                 ventura:       "2a3042edf8906e5906d28360b861482a7f4dda5864262e3bd6c864b6dbbba955"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7cd2241d2a9dae5d40380f6ca0205892922699bad0aa49591656076818b0d30a"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "googletest" => :build
+  depends_on "wangle" => :build
+  depends_on "boost"
+  depends_on "fb303"
+  depends_on "fbthrift"
+  depends_on "fmt"
+  depends_on "folly"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "openssl@3"
+
+  def install
+    # Fix "Process terminated due to timeout" by allowing a longer timeout.
+    inreplace buildpath.glob("eden/common/{os,utils}/test/CMakeLists.txt"),
+              /gtest_discover_tests\((.*)\)/,
+              "gtest_discover_tests(\\1 DISCOVERY_TIMEOUT 60)"
+    inreplace "eden/common/utils/test/CMakeLists.txt",
+              /gtest_discover_tests\((.*)\)/,
+              "gtest_discover_tests(\\1 DISCOVERY_TIMEOUT 60)"
+
+    # Avoid having to build FBThrift py library
+    inreplace "CMakeLists.txt", "COMPONENTS cpp2 py)", "COMPONENTS cpp2)"
+
+    shared_args = ["-DBUILD_SHARED_LIBS=ON", "-DCMAKE_INSTALL_RPATH=#{rpath}"]
+    linker_flags = %w[-undefined dynamic_lookup -dead_strip_dylibs]
+    linker_flags << "-ld_classic" if OS.mac? && MacOS.version == :ventura
+    shared_args << "-DCMAKE_SHARED_LINKER_FLAGS=-Wl,#{linker_flags.join(",")}" if OS.mac?
+
+    system "cmake", "-S", ".", "-B", "_build", *shared_args, *std_cmake_args
+    system "cmake", "--build", "_build"
+    system "cmake", "--install", "_build"
+  end
+
+  test do
+    (testpath/"test.cc").write <<~EOS
+      #include <eden/common/utils/ProcessInfo.h>
+      #include <cstdlib>
+      #include <iostream>
+
+      using namespace facebook::eden;
+
+      int main(int argc, char **argv) {
+        if (argc <= 1) return 1;
+        int pid = std::atoi(argv[1]);
+        std::cout << readProcessName(pid) << std::endl;
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++17", "-I#{include}", "test.cc",
+                    "-L#{lib}", "-L#{Formula["folly"].opt_lib}",
+                    "-L#{Formula["boost"].opt_lib}", "-L#{Formula["glog"].opt_lib}", "-L#{Formula["fmt"].opt_lib}",
+                    "-ledencommon_utils", "-lfolly", "-lfmt", "-lboost_context-mt", "-lglog", "-o", "test"
+    assert_match "ruby", shell_output("./test #{Process.pid}")
+  end
+end
